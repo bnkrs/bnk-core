58a594b72454b125f39e270e0c240f125e0f8efd
lschuermann <lschuermann@users.noreply.github.com>
2016-05-30 17:19:28 +0200

    Add zxcvbn for password-strength.

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 var pkg = require('../package.json');
+var cfg = require('../config');
 
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json({ 
-    app: pkg.name, 
+  res.json({
+    app: pkg.name,
     version: pkg.version,
+    config: {
+      zxcvbn_minscore: cfg.zxcvbn_minscore
+    }
   });
 });
 
