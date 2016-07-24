22e96b592210b41c3c4f98ae5ffa1b4f0ae6622c
lschuermann <leon-git@lschuermann.xyz>
2016-07-24 14:13:02 +0200

    Document /, fixing #6

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,25 @@ var cfg = require('../config');
 var express = require('express');
 var router = express.Router();
 
+/**
+ * @api {get} / API-Information
+ * @apiName Info
+ * @apiGroup 0
+ *
+ * @apiSuccess {String} app The name of the backend-app
+ * @apiSuccess {String} version The backend-version
+ * @apiSuccess {Object} config Various configuration parameters (see Example)
+ *
+ * @apiSuccessExample Success-Response
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "app": "bnk-core",
+ *       "version": "1.5.2-r3",
+ *       "config": {
+ *         "zxcvbn_minScore": 4
+ *       }
+ *     }
+ */
 router.get('/', function(req, res, next) {
   res.json({
     app: pkg.name,
